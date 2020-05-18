9d122e53384d0fae5bf92c624c19dd1e67a0a961
Neroware <neroware@freenet.de>
2020-05-18 11:57:30 +0200

    Tested Small Flow

--- custom_apps/small_flow.py
+++ custom_apps/small_flow.py
@@ -14,153 +14,111 @@ DESCRIPTION = "Very basic flow test."
 
 
 ### =========================== QUICK Advection Algorithm =========================== ###
-#import numpy as np
-#import tensorflow as tf
 prev_step_derivates = {}
 
-def quick_advection(field, velocity_field, dt, type_id="default"):
-    try:
-        # Grid Points 
-        # (!) data[y][x]
-        points = field.points
-        print("Sample Points:\n ", points.data, "\n")
-        # Grid Points values
-        values = field.sample_at(points.data)[0]
-        print("Sample values:\n", values, "\n")
+def quick_advection(field, velocity_field, dt, type="density"):
+    def quick_advection_density(density_field, velodity_field, dt):
         # Neighboring grid points have a distance of 1.0
-        deltaX = deltaY = 1.0
+        delta_x = delta_y = 1.0
 
-        # X- and Y-components for velocity = (u, v)
-        # Position: (i,j)
-        vel = velocity_field.sample_at(points.data)[0]
-        vel_v = np.array(list(map(lambda row: np.array(list(map(lambda pos: [pos[0]], row))), vel)))
-        vel_u = np.array(list(map(lambda row: np.array(list(map(lambda pos: [pos[1]], row))), vel)))
-        print("Velocity Grid (v, u) at (i,j):\n ", vel, "\n")
-        # Position: (i+0.5,j)
-        points2 = CenteredGrid( np.array(list(map(lambda row: np.array(list(map(lambda pos: pos + [0.0, 0.5], row))), points.data))) )
-        vel2 = velocity_field.sample_at(points2.data)[0]
-        vel2_u = np.array(list(map(lambda row: np.array(list(map(lambda pos: [pos[1]], row))), vel2)))
-        print("Sample Points (i+0.5,j):\n ", points2.data, "\n")
-        print("u at (i+0.5,j):\n ", vel2_u, "\n")
-        # Position: (i+0.5,j+1)
-        points3 = CenteredGrid( np.array(list(map(lambda row: np.array(list(map(lambda pos: pos + [1.0, 0.5], row))), points.data))) )
-        vel3 = velocity_field.sample_at(points3.data)[0]
-        vel3_u = np.array(list(map(lambda row: np.array(list(map(lambda pos: [pos[1]], row))), vel3)))
-        print("Sample Points (i+0.5,j+1):\n ", points3.data, "\n")
-        print("u at (i+0.5,j+1):\n ", vel3_u, "\n")
-        # u at Position: (i+0.5,j+0.5) # Equation (3)
-        vel4_u = 0.5 * (vel_u + vel2_u)
-        print("u at (i+0.5,j+0.5):\n", vel4_u, "\n")
-
-
-        raise NotImplementedError("QUICK Advection is WIP!")
-
-    except StaggeredSamplePoints:
-        unstacked = field.unstack()
-        advected = []
-        for index in range(0, len(unstacked)):
-            component = unstacked[index]
-            advected.append(quick_advection(component, velocity_field, dt, type_id + str(index)))
-        return field.with_data(advected)
-
-
-
-
-def quick_advection_old(field, velocity_field, dt, type_id="default"):
-    try:
+        # Sample points (i,j)
         points = field.points
-        # Field values
-        values = field.sample_at(points.data)[0]
-        # Neighboring grid points are having a distance of 1.0
-        deltaX = deltaY = 1.0
+        # Density values at sample points
+        density_data = density_field.sample_at(points.data)[0]
+        # Grid Dimensions in Sample Points
+        dim_y = len(density_data)
+        dim_x = len(density_data[0])
+        #print("Dimension in Sample Points: ", dim_x, ", ", dim_y)
 
-        # X- and Y-components for velocity = (u, v)
-        # Position: (i+0.5,j)
-        vel = velocity_field.sample_at(points.data)[0]
-        vel_v = np.array(list(map(lambda row: np.array(list(map(lambda pos: [pos[0]], row))), vel)))
-        vel_u = np.array(list(map(lambda row: np.array(list(map(lambda pos: [pos[1]], row))), vel)))
-        # Position: (i+0.5,j+1)
-        points2 = CenteredGrid( np.array(list(map(lambda row: np.array(list(map(lambda pos: pos + [1.0, 0.0], row))), points.data))) )
-        vel2 = velocity_field.sample_at(points2.data)[0]
-        vel2_u = np.array(list(map(lambda row: np.array(list(map(lambda pos: [pos[1]], row))), vel2)))
-        # u at Position: (i+0.5,j+0.5) # Equation (3)
-        vel3_u = 0.5 * (vel_u + vel2_u)
-        # Position: (i-1,j+0.5)
-        points4 = CenteredGrid( np.array(list(map(lambda row: np.array(list(map(lambda pos: pos + [0.5, -1.5], row))), points.data))) )
-        vel4 = velocity_field.sample_at(points4.data)[0]
-        vel4_v = np.array(list(map(lambda row: np.array(list(map(lambda pos: [pos[0]], row))), vel4)))
-        # Position: (i,j+0.5)
-        points5 = CenteredGrid( np.array(list(map(lambda row: np.array(list(map(lambda pos: pos + [0.5, -0.5], row))), points.data))) )
-        vel5 = velocity_field.sample_at(points5.data)[0]
-        vel5_v = np.array(list(map(lambda row: np.array(list(map(lambda pos: [pos[0]], row))), vel5)))
-        # Position: (i+1,j+0.5)
-        points6 = CenteredGrid( np.array(list(map(lambda row: np.array(list(map(lambda pos: pos + [0.5, 0.5], row))), points.data))) )
-        vel6 = velocity_field.sample_at(points6.data)[0]
-        vel6_v = np.array(list(map(lambda row: np.array(list(map(lambda pos: [pos[0]], row))), vel6)))
-        # v at Position: (i+0.5,j+0.5) # Equation (4)
-        vel3_v = vel6_v.copy()
-        for j in range(0, len(vel3_u)):
-            for i in range(0, len(vel3_u[0])):
-                u = vel3_u[j][i][0]
-                v1 = vel4_v[j][i][0]
-                v2 = vel5_v[j][i][0]
-                v3 = vel6_v[j][i][0]
-                if(u >= 0):
-                    v = [0.125 * v1 + 0.25 * v2 + 0.625 * v3]
-                    vel3_v[j][i] = v
+        # Get velocity values
+        # u at (i+0.5,j), v at (i,j+0.5)
+        vel_v, vel_u = velocity_field.unstack()
+        #print("Staggered grid velocity u: ", vel_u.data)
+        #print("Staggered grid velocity v: ", vel_v.data)
+        
+        # Interpolate density at (i+0.5,j) and (i,j+0.5)
+        staggered_density_x = []
+        staggered_density_y = []
+        for j in range(0, dim_y):
+            row = []
+            for i in range(0, dim_x + 1):
+                value1 = value2 = 0.0
+                if(i == 0):
+                    value2 = density_data[j][i][0]
+                elif(i == dim_x):
+                    value1 = density_data[j][i - 1][0]
                 else:
-                    v = [0.625 * v1 + 0.25 * v2 + 0.125 * v3]
-                    vel3_v[j][i] = v
+                    value1 = density_data[j][i - 1][0]
+                    value2 = density_data[j][i][0]
+                rho = 0.5 * (value1 + value2)
+                row.append(rho)
+            staggered_density_x.append(row)
+        for j in range(0, dim_y + 1):
+            row = []
+            for i in range(0, dim_x):
+                value1 = value2 = 0.0
+                if(j == 0):
+                    value2 = density_data[j][i][0]
+                elif(j == dim_y):
+                    value1 = density_data[j - 1][i][0]
+                else:
+                    value1 = density_data[j - 1][i][0]
+                    value2 = density_data[j][i][0]
+                rho = 0.5 * (value1 + value2)
+                row.append(rho)
+            staggered_density_y.append(row)
 
-        # Generate partial derivate field # Equation (2)
-        # In X-Direction
-        derivate_field_x = values.copy()
-        derivate_field_y = values.copy()
-        derivate_field_t = values.copy()
-        # Get value data at (i+0.5,j+0.5) and (i-0.5,j+0.5)
-        value_points1 = CenteredGrid( np.array(list(map(lambda row: np.array(list(map(lambda pos: pos + [0.5, 0.0], row))), points.data))) )
-        value_points2 = CenteredGrid( np.array(list(map(lambda row: np.array(list(map(lambda pos: pos + [0.5, -1.0], row))), points.data))) )
-        values1 = field.sample_at(value_points1.data)[0]
-        values2 = field.sample_at(value_points2.data)[0]
+        print("Density Data:\n", density_data)
+        print("Staggered Density Grid X: ", staggered_density_x)
+        print("Staggered Density Grid Y: ", staggered_density_y)
+        print("=====================================================\n")
 
-        # We need to solve dU/dt = -d(uU)/dx - d(vU)/dy
-        for j in range(0, len(values)):
-            for i in range(0, len(values[0])):
-                u1 = vel3_u[j][i][0]
-                v1 = vel3_v[j][i][0]
-                u2 = u1
-                v2 = v1
-                if(i > 0):
-                    u2 = vel3_u[j][i-1][0]
-                    v2 = vel3_v[j][i-1][0]
-                
-                value1 = values1[j][i][0]
-                value2 = values2[j][i][0]
-  
-                duUdx = (u1 * value1 - u2 * value2) / deltaX
-                dvUdy = (v1 * value1 - v2 * value2) / deltaY
-                derivate_field_x[j][i] = np.array([duUdx])
-                derivate_field_y[j][i] = np.array([dvUdy])
-                derivate_field_t[j][i] = -1.0 * derivate_field_x[j][i] - derivate_field_y[j][i]
+        # Calculate partial derviates dup/dx, dvp/dy and finally dp/dt with velocity (u, v) and density p
+        derivates_grid = np.zeros((dim_y, dim_x))
+        density_data_step = density_data.copy()
+        vel_u_data = vel_u.data[0]
+        vel_v_data = vel_v.data[0]
 
-        # Perform a timestep. If there is no previous timestep saved we do Euler, otherwise we do Adams-Bashforth
-        if(not type_id in prev_step_derivates):
-            data = values.copy()
-            for j in range(0, len(data)):
-                for i in range(0, len(data[0])):
-                    data[j][i] += derivate_field_t[j][i] * dt
-            prev_step_derivates[type_id] = derivate_field_t
-            return field.with_data(np.array([data]))
-        else:
-            raise NotImplementedError("Adams-Bashforth Timestep is WIP!")
+        for j in range(0, dim_y):
+            for i in range(0, dim_x):
+                # Discretize partial derivates
+                rho_x1 = staggered_density_x[j][i]
+                rho_x2 = staggered_density_x[j][i + 1]
+                if(i == 0 and j == 1):
+                    print("Density at (0.5, 1) and (1.5, 1): ", rho_x1, ", ", rho_x2)
+                u1 = vel_u_data[j][i][0]
+                u2 = vel_u_data[j][i + 1][0]
+                dupdx = (u2 * rho_x2 - u1 * rho_x1) / delta_x
+                if(i == 0 and j == 1):
+                    print("Density partial derivate at (1, 1): ", dupdx)
 
-    except StaggeredSamplePoints:
-        unstacked = field.unstack()
-        advected = []
-        for index in range(0, len(unstacked)):
-            component = unstacked[index]
-            advected.append(quick_advection(component, velocity_field, dt, type_id + str(index)))
-        return field.with_data(advected)
+                rho_y1 = staggered_density_y[j][i]
+                rho_y2 = staggered_density_y[j + 1][i]
+                v1 = vel_v_data[j][i][0]
+                v2 = vel_v_data[j + 1][i][0]
+                dvpdy = (v2 * rho_y2 - v1 * rho_y1) / delta_y
+
+                # Solve Advection Equation
+                dpdt = -dupdx - dvpdy
+                derivates_grid[j][i] = dpdt
+
+                # Perform Explicit Euler Step
+                density_data_step[j][i][0] += derivates_grid[j][i] * dt
+
+        return CenteredGrid([density_data_step])
+        #raise NotImplementedError("QUICK for Density is WIP!")
+
+
+    def quick_advection_velocity(velocity_field, dt):
+        raise NotImplementedError("QUICK for Velocity is WIP!")
+
+
+    if(type == "density"):
+        return quick_advection_density(field, velocity_field, dt)
+    elif(type == "velocity"):
+        return quick_advection_velocity(velocity_field, dt)
+
+    
 
 ### =========================== End QUICK =========================== ###
 
@@ -191,8 +149,8 @@ class SimpleFlow(App):
         dt = self.timestep
 
         # Advection
-        self.fluid.density = quick_advection(density, velocity, dt, type_id="density")
-        self.fluid.velocity = quick_advection(velocity, velocity, dt, type_id="velocity")
+        self.fluid.density = quick_advection(density, velocity, dt, type="density")
+        #self.fluid.velocity = quick_advection(velocity, velocity, dt, type="velocity")
 
         world.step(dt=self.timestep)
         
