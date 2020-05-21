56dddd315a4b69178b2d9005b5b2e46bd672c5bf
Neroware <neroware@freenet.de>
2020-05-21 12:42:33 +0200

    Added Leonardss Quadratic Upwind to density advection function

--- custom_apps/simple_flow.py
+++ custom_apps/simple_flow.py
@@ -33,63 +33,89 @@ def quick_advection(field, velocity_field, dt, type="density"):
         # Get velocity values
         # u at (i+0.5,j), v at (i,j+0.5)
         vel_v, vel_u = velocity_field.unstack()
+        vel_u_data = vel_u.data[0]
+        vel_v_data = vel_v.data[0]
         #print("Staggered grid velocity u: ", vel_u.data)
         #print("Staggered grid velocity v: ", vel_v.data)
         
-        # Interpolate density at (i+0.5,j) and (i,j+0.5)
+        # Upwind density at (i+0.5,j) and (i,j+0.5)
+        # Compare paper by Leonard
         staggered_density_x = []
         staggered_density_y = []
         for j in range(0, dim_y):
             row = []
             for i in range(0, dim_x + 1):
-                value1 = value2 = 0.0
-                if(i == 0):
-                    value2 = density_data[j][i][0]
-                elif(i == dim_x):
-                    value1 = density_data[j][i - 1][0]
+                if (vel_u_data[j][i] > 0):
+                    rho_R = rho_C = rho_L = 0.0
+                    if(i < dim_x):
+                        rho_R = density_data[j][i]
+                    if(i > 0):
+                        rho_C = density_data[j][i - 1]
+                    if(i > 1):
+                        rho_L = density_data[j][i - 2]
+                    rho = 0.5 * (rho_C + rho_R) - 0.125 * (rho_L + rho_R - 2.0 * rho_C)
+                    row.append(rho)
+                elif (vel_u_data[j][i] < 0):
+                    rho_R = rho_C = rho_FR = 0.0
+                    if(i < dim_x):
+                        rho_R = density_data[j][i]
+                    if(i < dim_x - 1):
+                        rho_FR = density_data[j][i + 1]
+                    if(i > 0):
+                        rho_C = density_data[j][i - 1]
+                    rho = 0.5 * (rho_C + rho_R) - 0.125 * (rho_FR + rho_C - 2.0 * rho_R)
+                    row.append(rho)
                 else:
-                    value1 = density_data[j][i - 1][0]
-                    value2 = density_data[j][i][0]
-                rho = 0.5 * (value1 + value2)
-                row.append(rho)
+                    row.append(0.0)
             staggered_density_x.append(row)
+        ### Y-Direction is WIP! I set everything to 0 because right now v = 0 anyways!
         for j in range(0, dim_y + 1):
             row = []
             for i in range(0, dim_x):
-                value1 = value2 = 0.0
-                if(j == 0):
-                    value2 = density_data[j][i][0]
-                elif(j == dim_y):
-                    value1 = density_data[j - 1][i][0]
+                if (vel_v_data[j][i] > 0):
+                    rho_R = rho_C = rho_L = 0.0
+                    if(j < dim_y):
+                        rho_R = density_data[j][i]
+                    if(j > 0):
+                        rho_C = density_data[j - 1][i]
+                    if(j > 1):
+                        rho_L = density_data[j - 2][i]
+                    rho = 0.5 * (rho_C + rho_R) - 0.125 * (rho_L + rho_R - 2.0 * rho_C)
+                    row.append(rho)
+                elif (vel_v_data[j][i] < 0):
+                    rho_R = rho_C = rho_FR = 0.0
+                    if(j < dim_y):
+                        rho_R = density_data[j][i]
+                    if(j < dim_y - 1):
+                        rho_FR = density_data[j + 1][i]
+                    if(j > 0):
+                        rho_C = density_data[j - 1][i]
+                    rho = 0.5 * (rho_C + rho_R) - 0.125 * (rho_FR + rho_C - 2.0 * rho_R)
+                    row.append(rho)
                 else:
-                    value1 = density_data[j - 1][i][0]
-                    value2 = density_data[j][i][0]
-                rho = 0.5 * (value1 + value2)
-                row.append(rho)
+                    row.append(0.0)
             staggered_density_y.append(row)
 
-        print("Density Data:\n", density_data)
-        print("Staggered Density Grid X: ", staggered_density_x)
-        print("Staggered Density Grid Y: ", staggered_density_y)
+        #print("Density Data:\n", density_data)
+        #print("Staggered Density Grid X: ", staggered_density_x)
+        #print("Staggered Density Grid Y: ", staggered_density_y)
 
         # Calculate partial derviates dup/dx, dvp/dy and finally dp/dt with velocity (u, v) and density p
         derivates_grid = np.zeros((dim_y, dim_x))
         density_data_step = density_data.copy()
-        vel_u_data = vel_u.data[0]
-        vel_v_data = vel_v.data[0]
 
         for j in range(0, dim_y):
             for i in range(0, dim_x):
                 # Discretize partial derivates
                 rho_x1 = staggered_density_x[j][i]
                 rho_x2 = staggered_density_x[j][i + 1]
-                if(i == 0 and j == 1):
-                    print("Density at (0.5, 1) and (1.5, 1): ", rho_x1, ", ", rho_x2)
+                #if(i == 0 and j == 1):
+                #    print("Density at (0.5, 1) and (1.5, 1): ", rho_x1, ", ", rho_x2)
                 u1 = vel_u_data[j][i][0]
                 u2 = vel_u_data[j][i + 1][0]
                 dupdx = (u2 * rho_x2 - u1 * rho_x1) / delta_x
-                if(i == 0 and j == 1):
-                    print("Density partial derivate at (1, 1): ", dupdx)
+                #if(i == 0 and j == 1):
+                #    print("Density partial derivate at (1, 1): ", dupdx)
 
                 rho_y1 = staggered_density_y[j][i]
                 rho_y2 = staggered_density_y[j + 1][i]
@@ -104,7 +130,7 @@ def quick_advection(field, velocity_field, dt, type="density"):
                 # Perform Explicit Euler Step
                 density_data_step[j][i][0] += derivates_grid[j][i] * dt
 
-        print("=====================================================\n")
+        #print("=====================================================\n")
         return CenteredGrid([density_data_step])
         #raise NotImplementedError("QUICK for Density is WIP!")
 
@@ -133,7 +159,7 @@ class SimpleFlow(App):
 
         self.physics = SimpleFlowPhysics()
         #self.physics = IncompressibleFlow()
-        self.timestep = 0.05
+        self.timestep = 0.25
 
         fluid = self.fluid = world.add(Fluid(Domain(RESOLUTION, box=box[0:100, 0:100], boundaries=OPEN), buoyancy_factor=0.0), physics=self.physics)
         fluid.velocity = self._get_velocity_grid()
@@ -191,7 +217,7 @@ class SimpleFlow(App):
         for y in range(0, RESOLUTION[0] + 1):
             next = []
             for x in range(0, RESOLUTION[0] + 1):
-                next.append([0.0, 1.0])
+                next.append([-1.0, 0.0])
             data.append(next)
 
         velocity_grid = np.array([data])
