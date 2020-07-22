50d9f4e2aaad307a72678a1a24bd8b6958969640
Raoul Zebisch <raoul.zebisch@tum.de>
2020-07-22 10:44:27 +0200

    Adding test cases for Velocity

--- custom_apps/test_suit/outputs/semi_lagrange/clean.sh
+++ custom_apps/test_suit/outputs/semi_lagrange/clean.sh
@@ -3,3 +3,4 @@ rm -r Escape_2/*
 rm -r Simple_Stream/*
 rm -r Sin_xy/*
 rm -r Sin_xy_2/*
+rm -r Center_1/*
--- custom_apps/test_suit/test_suit.py
+++ custom_apps/test_suit/test_suit.py
@@ -133,40 +133,7 @@ def plot_grid(data, dirname, filename, min_value, max_value):
         fig.colorbar(psm, ax=ax)
     fig.savefig("outputs/" + test_dir + "/" + dirname + "/" + filename)
     plt.close()
-
-
-#def colorbar_to_image(min_value, max_value, dir_name, case_name, file_name, descr):
-#    fig, ax = plt.subplots(figsize=(6, 1))
-#    fig.subplots_adjust(bottom=0.5)
-#
-#    cmap = mpl.cm.cool
-#    norm = mpl.colors.Normalize(vmin=min_value, vmax=max_value)
-#
-#    cb1 = mpl.colorbar.ColorbarBase(ax, cmap=cmap, norm=norm, orientation='horizontal')
-#    cb1.set_label(descr)
-#    fig.savefig("outputs/" + dir_name + "/" + case_name + "/" + file_name + ".jpg")
     
-
-
-
-#def array_to_image(arr, dirname, filename, min_value, max_value):
-#    test_dir = "quick"
-#    if semi_langrange_mode:
-#        test_dir = "semi_lagrange"
-#
-#    try:
-#        os.mkdir('outputs/' + test_dir + '/' + dirname)
-#    except OSError:
-#        pass
-#    
-#    img = []
-#    for row in arr:
-#        next = []
-#        for col in row:
-#            next.append(col[0])
-#        img.append(next) 
-#    plt.imsave('outputs/' + test_dir + '/' + dirname + '/' + filename, img, vmin=min_value, vmax=max_value)
-
 
 def run_test_cases(test_cases):
     for test_case in test_cases:
@@ -325,39 +292,39 @@ TEST_CASES.append(case_3)
 
 
 ### Case 4 ###
-#data = []
-#for y in range(0, RESOLUTION[0]):
-#    next = []
-#    for x in range(0, RESOLUTION[0]):
-#        vx = x - 50
-#        vy = y - 50
-#        if(vx == 0.0 and vy == 0.0):
-#            vx = 1
-#            vy = 1
-#        m = math.sqrt(vx * vx + vy * vy)
-#        if(m > 6.0):
-#            m = 0.0
-#        else:
-#            m = 1.0
-#        next.append([0.3 * m])
-#    data.append(next)
-#density_array = np.array([data], dtype="float32")
-#density_field = CenteredGrid(density_array)
-#
-#data = []
-#for y in range(0, RESOLUTION[0] + 1):
-#    next = []
-#    for x in range(0, RESOLUTION[0] + 1):
-#        next.append([0.005 * (y - 50), 0.005 * (x - 50)])
-#    data.append(next)
-#velocity_array = np.array([data], dtype="float32")
-#velocity_field = StaggeredGrid(velocity_array)
-#
-#if not semi_langrange_mode:
-#    case_4 = TestCase("Escape_2", velocity_array, density_array, 0.1)
-#else:
-#    case_4 = TestCase("Escape_2", velocity_field, density_field, 0.1)
-#TEST_CASES.append(case_4)
+data = []
+for y in range(0, RESOLUTION[0]):
+    next = []
+    for x in range(0, RESOLUTION[0]):
+        vx = x - 50
+        vy = y - 50
+        if(vx == 0.0 and vy == 0.0):
+            vx = 1
+            vy = 1
+        m = math.sqrt(vx * vx + vy * vy)
+        if(m > 6.0):
+            m = 0.0
+        else:
+            m = 1.0
+        next.append([0.3 * m])
+    data.append(next)
+density_array = np.array([data], dtype="float32")
+density_field = CenteredGrid(density_array)
+
+data = []
+for y in range(0, RESOLUTION[0] + 1):
+    next = []
+    for x in range(0, RESOLUTION[0] + 1):
+        next.append([0.005 * (y - 50), 0.005 * (x - 50)])
+    data.append(next)
+velocity_array = np.array([data], dtype="float32")
+velocity_field = StaggeredGrid(velocity_array)
+
+if not semi_langrange_mode:
+    case_4 = TestCase("Escape_2", velocity_array, density_array, 0.1)
+else:
+    case_4 = TestCase("Escape_2", velocity_field, density_field, 0.1)
+TEST_CASES.append(case_4)
 
 
 ### Case 5 ###
@@ -394,6 +361,43 @@ if not semi_langrange_mode:
 else:
     case_5 = TestCase("Simple_Stream", velocity_field, density_field, 0.1)
 TEST_CASES.append(case_5)
+
+
+### Case 6 ###
+data = []
+for y in range(0, RESOLUTION[0]):
+    next = []
+    for x in range(0, RESOLUTION[0]):
+        vx = x - 50
+        vy = y - 50
+        if(vx == 0.0 and vy == 0.0):
+            vx = 1
+            vy = 1
+        m = math.sqrt(vx * vx + vy * vy)
+        if(m > 6.0):
+            m = 0.0
+        else:
+            m = 1.0
+        next.append([0.3 * m])
+    data.append(next)
+density_array = np.array([data], dtype="float32")
+density_field = CenteredGrid(density_array)
+
+data = []
+for y in range(0, RESOLUTION[0] + 1):
+    next = []
+    for x in range(0, RESOLUTION[0] + 1):
+        next.append([-0.005 * (y - 50), -0.005 * (x - 50)])
+    data.append(next)
+velocity_array = np.array([data], dtype="float32")
+velocity_field = StaggeredGrid(velocity_array)
+
+if not semi_langrange_mode:
+    case_6 = TestCase("Center_1", velocity_array, density_array, 0.1)
+else:
+    case_6 = TestCase("Center_1", velocity_field, density_field, 0.1)
+TEST_CASES.append(case_6)
+
 
 #cProfile.run('run_test_cases(TEST_CASES)')
 run_test_cases(TEST_CASES)
