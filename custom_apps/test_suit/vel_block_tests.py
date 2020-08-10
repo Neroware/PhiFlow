f379b9203e05bce852852891a95f9d09b2d624e3
Raoul Zebisch <raoul.zebisch@tum.de>
2020-08-10 11:04:56 +0200

    Testing differences

--- custom_apps/test_suit/vel_block_tests.py
+++ custom_apps/test_suit/vel_block_tests.py
@@ -140,13 +140,13 @@ def plot_grid_diff_y_mirror(data1, data2, x_len, y_len, filename):
     for row in data1:
         next = []
         for col in row:
-            next.append(col[0])
+            next.append(col)
         diff.append(next)
 
     for j in range(0, y_len):
         j_idx = y_len - j - 1
         for i in range(0, x_len):
-            diff[j][i] -= diff[j_idx][i]
+            diff[j][i][0] -= data2[j_idx][i][0]
 
     plot_grid(diff, "diff", filename, -0.4, 0.4)
 
@@ -168,7 +168,7 @@ def run_test_cases(test_cases):
     v2_init = case_2.get_velocity_y()
     u2_init = case_2.get_velocity_x()
     den2_init = case_2.get_density()
-    plot_grid_diff_y_mirror(u1_init, u2_init, RESOLUTION[0] + 1, RESOLUTION[0], "diff_init.jpg")
+    plot_grid_diff_y_mirror(u1_init[0], u2_init[0], RESOLUTION[0] + 1, RESOLUTION[0], "diff_init.jpg")
 
     case_1.step()
     case_2.step()
@@ -179,7 +179,7 @@ def run_test_cases(test_cases):
     v2_1 = case_2.get_velocity_y()
     u2_1 = case_2.get_velocity_x()
     den2_1 = case_2.get_density()
-    plot_grid_diff_y_mirror(u1_1, u2_1, RESOLUTION[0] + 1, RESOLUTION[0], "diff_1.jpg")
+    plot_grid_diff_y_mirror(u1_1[0], u2_1[0], RESOLUTION[0] + 1, RESOLUTION[0], "diff_1.jpg")
 
     for count in range(0, 6):
         for step in range(0, 50):
@@ -191,7 +191,7 @@ def run_test_cases(test_cases):
         v2 = case_2.get_velocity_y()
         u2 = case_2.get_velocity_x()
         den2 = case_2.get_density()
-        plot_grid_diff_y_mirror(u1, u2, RESOLUTION[0] + 1, RESOLUTION[0], "diff_" + ((count + 1) * 50) + ".jpg")
+        plot_grid_diff_y_mirror(u1[0], u2[0], RESOLUTION[0] + 1, RESOLUTION[0], "diff_" + str((count + 1) * 50) + ".jpg")
 
 
 TEST_CASES = []
