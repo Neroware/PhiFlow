a3720a6b84db380442a8f34954cb923e805ba50d
Raoul Zebisch <raoul.zebisch@tum.de>
2020-08-10 09:40:43 +0200

    Testing Vel XY Block 1

--- phi/tf/cuda/src/quick_advection_op.cu.cc
+++ phi/tf/cuda/src/quick_advection_op.cu.cc
@@ -195,7 +195,7 @@ __global__ void advectVelocityYQuick(float* output_field, float* u, float* v, in
         v4 = 0.5f * (v_C + v_R) - 0.125f * (v_FR + v_C - 2.0f * v_R);
     }
     float delta_v_v_delta_y = v4 * v4 - v3 * v3;
-    float delta_v_delta_t = -delta_u_v_delta_x - delta_v_v_delta_y;
+    float delta_v_delta_t = /*-delta_u_v_delta_x*/ - delta_v_v_delta_y;
     output_field[IDX(j, i, dim)] = v[pidx(j, i, dim, padding)] + delta_v_delta_t * dt;
 }
 
@@ -286,7 +286,7 @@ __global__ void advectVelocityXQuick(float* output_field, float* u, float* v, in
     }
     float delta_u_u_delta_x = u4 * u4 - u3 * u3;
     float delta_u_delta_t = -delta_u_u_delta_x - delta_v_u_delta_y;
-    output_field[IDX(j, i, dim + 1)] = u[pidx(j, i, dim + 1, padding)] + delta_u_delta_t * dt;
+    output_field[IDX(j, i, dim + 1)] = u[pidx(j, i, dim + 1, padding)] /*+ delta_u_delta_t * dt*/;
 }
 
 
